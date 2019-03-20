269c78f1b0ef774d4e4d18b323e88bd457c754c0
= <=>
2019-03-20 20:31:23 +0700

    fix slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,30 +1,8 @@
-//slider
-      //khai báo biến slideIndex đại diện cho slide hiện tại
-      var slideIndex;
-      // KHai bào hàm hiển thị slide
-      function showSlides() {
-          var i;
-          var slides = document.getElementsByClassName("s-content");
-          var dots = document.getElementsByClassName("vongtronnho");
-          for (i = 0; i < slides.length; i++) {
-             slides[i].style.display = "none";  
-          }
-          for (i = 0; i < dots.length; i++) {
-              dots[i].className = dots[i].className.replace(" active", "");
-          }
-
-          slides[slideIndex].style.display = "block";  
-          dots[slideIndex].className += " active";
-          //chuyển đến slide tiếp theo
-          slideIndex++;
-          //nếu đang ở slide cuối cùng thì chuyển về slide đầu
-          if (slideIndex > slides.length - 1) {
-            slideIndex = 0
-          }    
-          //tự động chuyển đổi slide sau 5s       
-      }
-      //mặc định hiển thị slide đầu tiên 
-      showSlides(slideIndex = 0);
-      function currentSlide(n) {
-        showSlides(slideIndex = n);
-      }
+$(document).ready(function(){
+    $('#review-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true
+    });
+});
