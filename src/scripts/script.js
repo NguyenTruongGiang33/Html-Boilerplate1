20aa6a7c93ad53d0434dfe8befa6f14af2efe280
= <=>
2019-03-22 08:39:14 +0700

    Mất js và fontawe header

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
         dots: true
     });
 });
+
+// không hiểu sao lại mất đoạn js của header 
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var opacity = 0;
+        if (scroll < 100) {
+            opacity = scroll / 100;
+            $("#b-page-header").css('background-color', 'rgba(75, 193, 210, ' + opacity + ')');
+        } else {
+            opacity = 1;
+            $("#b-page-header").css('background-color', 'rgba(75, 193, 210, ' + opacity + ')')
+        }
+    })
+});
